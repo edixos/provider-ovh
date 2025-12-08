44fcfdf098fb7b99e53104b1e1c758a3199ce314
ekarlso <endre.karlson@xait.com>
2025-12-08 10:28:03 +0100

    Expose access_key_id into connection secret

--- config/cluster/cloud/config.go
+++ config/cluster/cloud/config.go
@@ -31,6 +31,15 @@ func Configure(p *config.Provider) {
 		r.References["user_id"] = config.Reference{
 			TerraformName: "ovh_cloud_project_user",
 		}
+
+		r.Sensitive.AdditionalConnectionDetailsFn = func(attr map[string]any) (map[string][]byte, error) {
+			details := make(map[string][]byte)
+			if accessKey, ok := attr["access_key_id"].(string); ok {
+				details["access_key_id"] = []byte(accessKey)
+			}
+
+			return details, nil
+		}
 	})
 	p.AddResourceConfigurator("ovh_cloud_project_user_s3_policy", func(r *config.Resource) {
 		r.ShortGroup = shortGroup
@@ -58,6 +67,9 @@ func Configure(p *config.Provider) {
 		r.ShortGroup = shortGroup
 	})
 	p.AddResourceConfigurator("ovh_cloud_project_region", func(r *config.Resource) {
+		r.ShortGroup = shortGroup
+	})
+	p.AddResourceConfigurator("ovh_cloud_project_storage", func(r *config.Resource) {
 		r.ShortGroup = shortGroup
 	})
 }
